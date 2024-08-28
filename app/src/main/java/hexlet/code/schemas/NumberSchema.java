05fd508893e9ebc9e8a9c453a783e7304d84c1d1
Ddm14159 <ddm14159@gmail.com>
2024-08-28 19:56:18 +0300

    validation fix

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,17 +1,18 @@
 package hexlet.code.schemas;
 
+import java.util.Objects;
 import java.util.function.Function;
 
 public class NumberSchema extends BaseSchema<Integer> {
     public NumberSchema required() {
-        Function<Integer, Boolean> fn = (v) -> v != null;
+        Function<Integer, Boolean> fn = Objects::nonNull;
         this.addRule("required", fn);
 
         return this;
     }
 
     public NumberSchema positive() {
-        Function<Integer, Boolean> fn = (v) -> v > 0;
+        Function<Integer, Boolean> fn = (v) -> v == null || v > 0;
         this.addRule("positive", fn);
 
         return this;
